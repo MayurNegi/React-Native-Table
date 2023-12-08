adcae97aa761822a2110c5122174a630627e5796
bigfudge99 <mayurnegi1999@gmail.com>
2023-12-08 13:55:08 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppWrapper from './src';
+
+function App(): React.JSX.Element {
+  return <AppWrapper />;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function AppWrapper(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Hi</Text>
+    </SafeAreaView>
+  );
+}
