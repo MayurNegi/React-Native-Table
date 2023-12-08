4dc1bcc5639734f9cf60e4af6075ea7a9eb312ba
bigfudge99 <mayurnegi1999@gmail.com>
2023-12-08 15:47:07 +0530

    add page number and handle next and back

--- src/features/table/index.tsx
+++ src/features/table/index.tsx
@@ -1,15 +1,60 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {data} from '../../data/data';
-import {FlatList, ScrollView, View} from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+} from 'react-native';
 import Column from '../column';
 import TableHeader from './TableHeader';
 
+const PAGE_LENGTH = 25;
+
 const Table = () => {
-  const [table, setTable] = useState(data.slice(0, 25));
-  console.log('table', table);
+  const [table, setTable] = useState(data.slice(0, PAGE_LENGTH));
+  let pageNumberRef = useRef(0);
+
+  const handleNext = () => {
+    const newPageIndex = pageNumberRef.current + 1;
+    pageNumberRef.current = newPageIndex;
+    const firstElementIndex = newPageIndex * PAGE_LENGTH;
+    setTable(data.slice(firstElementIndex, firstElementIndex + PAGE_LENGTH));
+  };
+
+  const handleBack = () => {
+    if (pageNumberRef?.current === 0) {
+      return;
+    }
+
+    const newPageIndex = pageNumberRef.current - 1;
+    pageNumberRef.current = newPageIndex;
+    const firstElementIndex = newPageIndex * PAGE_LENGTH;
+    setTable(data.slice(firstElementIndex, firstElementIndex + PAGE_LENGTH));
+  };
 
   return (
-    <View style={{flex: 1}}>
+    <ScrollView style={{flex: 1}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 16,
+          marginVertical: 16,
+        }}>
+        <Text>Page No.: {pageNumberRef.current}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Pressable onPress={handleBack} style={styles.button}>
+            <Text style={{textAlign: 'center', paddingVertical: 6}}>Back</Text>
+          </Pressable>
+          <Pressable onPress={handleNext} style={styles.button}>
+            <Text style={{textAlign: 'center', paddingVertical: 6}}>Next</Text>
+          </Pressable>
+        </View>
+      </View>
+
       <ScrollView horizontal>
         <View style={{display: 'flex', flexDirection: 'column'}}>
           <TableHeader />
@@ -21,7 +66,16 @@ const Table = () => {
           />
         </View>
       </ScrollView>
-    </View>
+    </ScrollView>
   );
 };
 export default Table;
+
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderRadius: 4,
+    width: 80,
+    marginHorizontal: 8,
+  },
+});
